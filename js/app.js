5db26400e9b2228cdddad99602e92311eceb9fa8
Allison Amaral <amaralli@uw.edu>
2014-12-03 23:54:27 -0800

    lifjds

--- js/app.js
+++ js/app.js
@@ -12,16 +12,15 @@ angular.module('RatingApp', ['ui.bootstrap'])
 		$httpProvider.defaults.headers.common["X-Parse-REST-API-Key"] = 'd2hH5a1zBhtuLQv38oo57KOPhWW0rQgz5xoyB0Oz';
 	})
 
-	.controller('RatingController', function($scope, $http) {
-
-
+	.controller('CommentController', function($scope, $http) {
 
 		$scope.refreshComments = function() {
             console.log("log 1");
-			$http.get(parseURL)
+			$http.get(parseURL + "?order=-score")
 				.success(function(data) {
 					$scope.comments = data.results;
 					console.log("refreshed!");
+					console.log(data.results);
 				})
 				.error(function (err) {
                 console.log("log 2cvcvcvcv");
@@ -31,21 +30,26 @@ angular.module('RatingApp', ['ui.bootstrap'])
 					$scope.updating = false;
 				});
 		};
+		$scope.refreshComments();
 
-        console.log("FUCKSOIHRWKHRKJJ");
+
 		$scope.postComment = function () {
             console.log("log 1");
+            var newComment = {
+            	score: 0
+        	};
 			$scope.inserting = true;
 			$http.post(parseURL, $scope.newComment)
 				.success(function(responseData) {
                     console.log("log 2");
-					$scope.newComment.objectId = responseData.objectId;
+					newComment.objectId = responseData.objectId;
                     console.log("log 3");
 					$scope.comments.push($scope.newComment);
                     console.log("log 4");
+                    $scope.newComment = {done: false};
 				})
 				.error(function (err) {
-					console.log("FUCK -> " + err);
+					console.log(err);
                     console.log($scope.newComment);
 				})
 				.finally(function() {
@@ -53,21 +57,45 @@ angular.module('RatingApp', ['ui.bootstrap'])
 					$scope.inserting = false;
 				});
 		};
-        console.log("qqq1");
-		$scope.refreshComments();
+
+        $scope.updateComments = function(comment) {
+        	$http.put(parseURL + "/" + comment.objectId, comment)
+        		.success(function() {
+        			console.log("successfully updated");
+        		});
+        };
+
+        $scope.incrementScore = function(comment, increment) {
+        	var postData = {
+        		score: {
+	        		__op: "Increment",
+	        		amount: increment
+        		}	
+        	};
+
+        	$scope.updating = true;
+        	$http.put(parseURL + '/' + comment.objectId, postData)
+        		.success(function (respData) {
+        			comment.score = respData.score;
+        		})
+        		.error(function(error){
+        			console.log(error)
+        		})
+        		.finally(function(){
+        			$scope.updating = false;
+        		});
+        };
 
 		$scope.deleteComment = function(comment) {
 			$http.delete(parseURL + "/" + comment.objectId)
-				.success(function (data) {
+				.success(function () {
 					console.log("Comment Successfully Deleted");
+					$scope.refreshComments();
 				})
 				.error(function (err) {
-					console.log("FUCK -> " + err);
+					console.log(err);
 				});
 		};
-
-		//$scope.
-        console.log("qqq2");
 
 	});
 
